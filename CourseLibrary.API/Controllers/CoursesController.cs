08480c96c1eedab465350731ae22764a04f79269
Irma Castillo <Irma.Castillo@healthsun.com>
2019-12-16 17:13:58 -0500

    Vaidating resources

--- CourseLibrary.API/Controllers/CoursesController.cs
+++ CourseLibrary.API/Controllers/CoursesController.cs
@@ -88,5 +88,29 @@ namespace CourseLibrary.API.Controllers
             }
         }
 
+        [HttpPut("{courseId}")]
+        public ActionResult UpdateCourseForAuthor(Guid authorId, Guid courseId, CourseForUpdateDto course)
+        {
+            if (!_courseLibraryRepository.AuthorExists(authorId))
+            {
+                return NotFound();
+            }
+
+            var courseForAuthorFromRepo = _courseLibraryRepository.GetCourse(authorId, courseId);
+
+            if (courseForAuthorFromRepo is null)
+            {  
+                return NotFound();
+            }
+                                                          
+            //map the entity to a CourseForUpdateDto      
+            //apply the updated field values to that dto  
+            //map the CourseFrUpdateDto back to an entity 
+            _mapper.Map(course, courseForAuthorFromRepo); 
+            _courseLibraryRepository.UpdateCourse(courseForAuthorFromRepo);
+            _courseLibraryRepository.Save();
+            return NoContent();
+        }
+
     }
 }

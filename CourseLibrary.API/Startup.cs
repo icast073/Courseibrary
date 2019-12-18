0b2d42efd680219cec56214b0cf8e141e8bd5db9
Irma Castillo <Irma.Castillo@healthsun.com>
2019-12-18 15:18:15 -0500

    Implementing patch with json package Patch

--- CourseLibrary.API/Startup.cs
+++ CourseLibrary.API/Startup.cs
@@ -1,4 +1,3 @@
-using System;
 using AutoMapper;
 using CourseLibrary.API.DbContexts;
 using CourseLibrary.API.Services;
@@ -10,6 +9,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json.Serialization;
+using System;
 
 namespace CourseLibrary.API
 {
@@ -25,38 +26,47 @@ namespace CourseLibrary.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-           services.AddControllers(setupAction =>
-           {
-               //if it is set to false, the API will respond resources in the default supported format, by default is false
-               setupAction.ReturnHttpNotAcceptable = true;
-           }).AddXmlDataContractSerializerFormatters()
+            services.AddControllers(setupAction =>
+            {
+                setupAction.ReturnHttpNotAcceptable = true;
 
-           .ConfigureApiBehaviorOptions(setupAction => setupAction.InvalidModelStateResponseFactory = context =>
-               {
-                   var problemDetails = new ValidationProblemDetails(context.ModelState)
-                   {
-                       Type = "https://courselibrary.com/modelvalidationproblem",
-                       Title = "One or more model validation error occured",
-                       Status = StatusCodes.Status422UnprocessableEntity,
-                       Detail = "See error for details.",
-                       Instance = context.HttpContext.Request.Path
-                   };
+            }).AddNewtonsoftJson(setupAction =>
+            {
+                setupAction.SerializerSettings.ContractResolver =
+                   new CamelCasePropertyNamesContractResolver();
+            })
+             .AddXmlDataContractSerializerFormatters()
+            .ConfigureApiBehaviorOptions(setupAction =>
+            {
+                setupAction.InvalidModelStateResponseFactory = context =>
+                {
+                    var problemDetails = new ValidationProblemDetails(context.ModelState)
+                    {
+                        Type = "https://courselibrary.com/modelvalidationproblem",
+                        Title = "One or more model validation errors occurred.",
+                        Status = StatusCodes.Status422UnprocessableEntity,
+                        Detail = "See the errors property for details.",
+                        Instance = context.HttpContext.Request.Path
+                    };
 
-                   problemDetails.Extensions.Add("traceId", context.HttpContext.TraceIdentifier);
-                   return new UnprocessableEntityObjectResult(problemDetails)
-                   {
-                       ContentTypes = { "application/problem+json" }
-                   };
-               });
+                    problemDetails.Extensions.Add("traceId", context.HttpContext.TraceIdentifier);
 
-           services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-           services.AddScoped<ICourseLibraryRepository, CourseLibraryRepository>();
+                    return new UnprocessableEntityObjectResult(problemDetails)
+                    {
+                        ContentTypes = { "application/problem+json" }
+                    };
+                };
+            });
 
-           services.AddDbContext<CourseLibraryContext>(options =>
-           {
-              options.UseSqlServer(
-                   @"Server=(localdb)\mssqllocaldb;Database=CourseLibraryDB;Trusted_Connection=True;");
-           }); 
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            services.AddScoped<ICourseLibraryRepository, CourseLibraryRepository>();
+
+            services.AddDbContext<CourseLibraryContext>(options =>
+            {
+                options.UseSqlServer(
+                    @"Server=(localdb)\mssqllocaldb;Database=CourseLibraryDB;Trusted_Connection=True;");
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -65,6 +75,18 @@ namespace CourseLibrary.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler(appBuilder =>
+                {
+                    appBuilder.Run(async context =>
+                    {
+                        context.Response.StatusCode = 500;
+                        await context.Response.WriteAsync("An unexpected fault happened. Try again later.");
+                    });
+                });
+
             }
 
             app.UseRouting();

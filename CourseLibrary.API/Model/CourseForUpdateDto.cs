c97d4cd1167263178da51c4c98352c32520ad480
Irma Castillo <Irma.Castillo@healthsun.com>
2019-12-19 14:25:46 -0500

    pagination

---
+++ CourseLibrary.API/Model/CourseForUpdateDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CourseLibrary.API.Model
+{
+    public class CourseForUpdateDto : CourseForManipulationDto
+    {
+        [Required(ErrorMessage = "You should fill out a description.")]
+        public override string Description { get => base.Description; set => base.Description = value; }
+
+    }
+}
